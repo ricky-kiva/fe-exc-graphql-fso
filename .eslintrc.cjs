30d9528ecb9d531011ebfc334d392bab771b80e2
ricky-kiva <ricky.kiva@gmail.com>
2025-09-13 00:05:47 +0700

    feat(lint): incl. TS support on `ESLint` config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,25 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint'
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'semi': ['error', 'always']
   },
 }
