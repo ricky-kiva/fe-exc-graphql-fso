3f267dfe073e664f1ffbf2c7aa7ac1c6508eccb7
ricky-kiva <ricky.kiva@gmail.com>
2025-09-13 15:46:59 +0700

    feat(exc-8.11-8.12): impl. `editAuthor` mutation

--- src/components/Authors.tsx
+++ src/components/Authors.tsx
@@ -1,12 +1,18 @@
-import { useQuery } from '@apollo/client/react';
-import React from "react";
+import { useMutation, useQuery } from '@apollo/client/react';
+import React, { useState, type ChangeEvent, type FormEvent } from "react";
 import { ALL_AUTHORS } from '../graphql/operations/queries/author';
 import type { AllAuthorsData } from '../graphql/types/data/author';
 import type { Author } from '../types/Author';
+import type { EditAuthorParam } from '../graphql/types/params/author';
+import { EDIT_AUTHOR } from '../graphql/operations/mutations/author';
 
 interface AuthorsProps {
   show: boolean;
 }
+
+interface SetAuthorBornProps {
+  authors: Author[];
+} 
 
 const Authors: React.FC<AuthorsProps> = (props) => {
   const result = useQuery<AllAuthorsData>(ALL_AUTHORS, {
@@ -44,6 +50,84 @@ const Authors: React.FC<AuthorsProps> = (props) => {
           ))}
         </tbody>
       </table>
+
+      <SetAuthorBorn authors={authors} />
+    </div>
+  );
+};
+
+const SetAuthorBorn: React.FC<SetAuthorBornProps> = (props) => {
+  const [name, setName] = useState<string>("");
+  const [born, setBorn] = useState<number>(0);
+
+  const [ editAuthor ] = useMutation<Author, EditAuthorParam>(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  });
+
+  const validateSubmit = (): boolean => {
+    if (!name) {
+      alert("Name is required");
+      return false;
+    }
+
+    if (!born) {
+      alert("Birth Year is required");
+      return false;
+    }
+
+    return true;
+  };
+
+  const submit = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+  
+      if (!validateSubmit()) return;
+  
+      try {
+        editAuthor({
+          variables: { name, born }
+        });
+  
+        setName("");
+        setBorn(0);
+  
+        alert("Author updated successfully!");
+      } catch(e: unknown) {
+        if (e instanceof Error) {
+          alert(`Failed to update author: ${e.message}`);
+        }
+      }
+    };
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <select
+          value={name}
+          onChange={(e: ChangeEvent<HTMLSelectElement>) => setName(e.target.value)}
+          >
+            <option value="">-- Select an author --</option>
+            {props.authors.map((a) => (
+              <option key={a.id} value={a.name}>{a.name}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          published
+          <input
+            type="number"
+            value={born}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              const val = e.target.value;
+              if (/^\d*$/.test(val)) setBorn(val === "" ? 0 : Number(val));
+            }}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
