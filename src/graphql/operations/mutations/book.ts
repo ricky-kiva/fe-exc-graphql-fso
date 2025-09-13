663225a2f5537e219a9c5903617df5bc8b7b01eb
ricky-kiva <ricky.kiva@gmail.com>
2025-09-13 15:46:17 +0700

    refactor(gql): capitalize `AddBook` mutation name

--- src/graphql/operations/mutations/book.ts
+++ src/graphql/operations/mutations/book.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const ADD_BOOK = gql`
-  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
     addBook(title: $title, author: $author, published: $published, genres: $genres) {
       id
       title
